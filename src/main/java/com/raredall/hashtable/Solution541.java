8622130d4bc37957a4ad3501e41630477e0df827
Raredall <raredall@outlook.com>
2022-04-09 17:17:07 +0800

    541. 反转字符串 II

---
+++ src/main/java/com/raredall/hashtable/Solution541.java
@@ -0,0 +1,27 @@
+package com.raredall.hashtable;
+
+/**
+ * 541. 反转字符串 II
+ *
+ * @author Raredall
+ */
+public class Solution541 {
+
+    public String reverseStr(String s, int k) {
+        int length = s.length();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < length; i += 2 * k) {
+            int left = i;
+            int right = Math.min(i + k, length) - 1;
+            while (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+}
