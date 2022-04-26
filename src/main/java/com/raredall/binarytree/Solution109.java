cee1be54403c1c73c25a302f9ed2cf26a48b9644
Raredall <raredall@outlook.com>
2022-04-27 00:36:46 +0800

    199. 二叉树的右视图

---
+++ src/main/java/com/raredall/binarytree/Solution109.java
@@ -0,0 +1,64 @@
+package com.raredall.binarytree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 199. 二叉树的右视图
+ *
+ * @author Raredall
+ */
+public class Solution109 {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 1; i <= size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i == size) {
+                    res.add(node.val);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
