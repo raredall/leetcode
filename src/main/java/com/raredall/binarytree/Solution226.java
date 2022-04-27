a61c637ed1987be409af4c3f1a7ed7007a5c94ae
Raredall <raredall@outlook.com>
2022-04-28 01:36:38 +0800

    226. 翻转二叉树

---
+++ src/main/java/com/raredall/binarytree/Solution226.java
@@ -0,0 +1,47 @@
+package com.raredall.binarytree;
+
+/**
+ * 226. 翻转二叉树
+ *
+ * @author Raredall
+ */
+public class Solution226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
