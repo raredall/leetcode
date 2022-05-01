3b3ccce451a55b93f7df806d0cf0a027c3821015
Raredall <raredall@outlook.com>
2022-05-01 21:42:54 +0800

    101. 对称二叉树(迭代法)

---
+++ src/main/java/com/raredall/binarytree/Solution101Iteration.java
@@ -0,0 +1,59 @@
+package com.raredall.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 101. 对称二叉树
+ *
+ * @author Raredall
+ */
+public class Solution101Iteration {
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null || left.val != right.val) {
+                return false;
+            }
+            queue.offer(left.left);
+            queue.offer(right.right);
+            queue.offer(left.right);
+            queue.offer(right.left);
+        }
+
+        return true;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
