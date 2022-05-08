a4c4fe780176056935fdcae97c24f31ddcebce62
Raredall <raredall@outlook.com>
2022-05-08 18:00:23 +0800

    110. 平衡二叉树

---
+++ src/main/java/com/raredall/binarytree/Solution110.java
@@ -0,0 +1,55 @@
+package com.raredall.binarytree;
+
+/**
+ * 110. 平衡二叉树
+ *
+ * @author Raredall
+ */
+public class Solution110 {
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
