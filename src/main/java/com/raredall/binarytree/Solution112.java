958646d9cef6e840dd7cad4c7c4d6e0fd15f256f
Raredall <raredall@outlook.com>
2022-07-13 01:09:55 +0800

    112. 路径总和

---
+++ src/main/java/com/raredall/binarytree/Solution112.java
@@ -0,0 +1,44 @@
+package com.raredall.binarytree;
+
+/**
+ * 112. 路径总和
+ *
+ * @author Raredall
+ */
+public class Solution112 {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
