9e052156ed9eda6dd5aac8cde2b771341b586fb1
Raredall <raredall@outlook.com>
2022-07-26 00:49:23 +0800

    617. 合并二叉树

---
+++ src/main/java/com/raredall/binarytree/Solution617.java
@@ -0,0 +1,48 @@
+package com.raredall.binarytree;
+
+/**
+ * 617. 合并二叉树
+ *
+ * @author Raredall
+ */
+public class Solution617 {
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left,root2.left);
+        root1.right = mergeTrees(root1.right,root2.right);
+
+        return root1;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
