209af5bf08a24481575168a82bf8f67fdb200ca6
Raredall <raredall@outlook.com>
2022-08-06 16:27:22 +0800

    236. 二叉树的最近公共祖先

---
+++ src/main/java/com/raredall/binarytree/Solution236.java
@@ -0,0 +1,50 @@
+package com.raredall.binarytree;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ *
+ * @author Raredall
+ */
+public class Solution236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        return right;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
