9b5703255b59e1715528ed80f53634f56742f644
Raredall <raredall@outlook.com>
2022-08-10 23:58:42 +0800

    450. 删除二叉搜索树中的节点

---
+++ src/main/java/com/raredall/binarytree/Solution450.java
@@ -0,0 +1,60 @@
+package com.raredall.binarytree;
+
+/**
+ * 450. 删除二叉搜索树中的节点
+ *
+ * @author Raredall
+ */
+public class Solution450 {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            TreeNode cur = root.right;
+            while (cur.left != null) {
+                cur = cur.left;
+            }
+            cur.left = root.left;
+            return root.right;
+        }
+
+        return root;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
